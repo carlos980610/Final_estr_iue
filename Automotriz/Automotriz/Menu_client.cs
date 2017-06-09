7c24956289212879c81e95494612c65dcbf5483c
carlos980610 <capianomusical@gmail.com>
2017-06-09 06:21:53 -0500

    Carga 23

--- Automotriz/Automotriz/Menu_client.cs
+++ Automotriz/Automotriz/Menu_client.cs
@@ -20,21 +20,11 @@ namespace Automotriz
 
         private void Menu_client_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void btnSalir_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void Menu_client_Load_1(object sender, EventArgs e)
-        {
             try
             {
                 SqlConnection conn = DataBaseConnection.DataBase_Open_Connection();
 
-                
+
                 var query = "select IdClient, Client_name, Client_phone, Client_user, Client_address, Client_id_card from tblClient where IdClient = '" + "" + "'";
 
                 SqlCommand command = new SqlCommand(query, conn);
@@ -51,6 +41,16 @@ namespace Automotriz
             {
 
             }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Menu_client_Load_1(object sender, EventArgs e)
+        {
+            
         }
     }
 }
