b5534d129370f3b971558f8c1403b6675061b805
Juan José Carvajal Agudelo <juanjosecarvajal123@gmail.com>
2017-06-11 23:11:55 -0500

    Carga 49

--- Automotriz/Automotriz/Part_add.Designer.cs
+++ Automotriz/Automotriz/Part_add.Designer.cs
@@ -30,8 +30,12 @@
         {
             this.btnExit = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblid = new System.Windows.Forms.Label();
+            this.lblPart = new System.Windows.Forms.Label();
+            this.lblPart_valor = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnExit
@@ -44,31 +48,71 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(216, 57);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Añadir";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Location = new System.Drawing.Point(19, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Parte :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Valor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Id:";
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(66, 21);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(22, 13);
+            this.lblid.TabIndex = 5;
+            this.lblid.Text = "xxx";
+            // 
+            // lblPart
+            // 
+            this.lblPart.AutoSize = true;
+            this.lblPart.Location = new System.Drawing.Point(66, 58);
+            this.lblPart.Name = "lblPart";
+            this.lblPart.Size = new System.Drawing.Size(22, 13);
+            this.lblPart.TabIndex = 6;
+            this.lblPart.Text = "xxx";
+            // 
+            // lblPart_valor
+            // 
+            this.lblPart_valor.AutoSize = true;
+            this.lblPart_valor.Location = new System.Drawing.Point(66, 98);
+            this.lblPart_valor.Name = "lblPart_valor";
+            this.lblPart_valor.Size = new System.Drawing.Size(22, 13);
+            this.lblPart_valor.TabIndex = 7;
+            this.lblPart_valor.Text = "xxx";
             // 
             // Part_add
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblPart_valor);
+            this.Controls.Add(this.lblPart);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnExit);
             this.Name = "Part_add";
             this.Text = "Part_add";
@@ -82,7 +126,11 @@
 
         private System.Windows.Forms.Button btnExit;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Label lblPart;
+        private System.Windows.Forms.Label lblPart_valor;
     }
 }
