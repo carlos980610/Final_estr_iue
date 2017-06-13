8d7cfe0d76214626342650390cbc8c24f30e8f39
Juan José Carvajal Agudelo <juanjosecarvajal123@gmail.com>
2017-06-13 18:51:54 -0500

    Carga 66

---
+++ Automotriz/Automotriz/Documentos/Workshop_status.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Automotriz
+{
+    partial class Workshop_status
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbService_Type = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtActual_Porcent = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipo de servicio";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(563, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cmbService_Type
+            // 
+            this.cmbService_Type.FormattingEnabled = true;
+            this.cmbService_Type.Items.AddRange(new object[] {
+            "Mantenimiento",
+            "Reparación"});
+            this.cmbService_Type.Location = new System.Drawing.Point(107, 59);
+            this.cmbService_Type.Name = "cmbService_Type";
+            this.cmbService_Type.Size = new System.Drawing.Size(121, 21);
+            this.cmbService_Type.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(626, 57);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Porcentaje de progreso";
+            // 
+            // txtActual_Porcent
+            // 
+            this.txtActual_Porcent.Location = new System.Drawing.Point(366, 59);
+            this.txtActual_Porcent.Name = "txtActual_Porcent";
+            this.txtActual_Porcent.Size = new System.Drawing.Size(100, 20);
+            this.txtActual_Porcent.TabIndex = 5;
+            // 
+            // Workshop_status
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 248);
+            this.Controls.Add(this.txtActual_Porcent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbService_Type);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Workshop_status";
+            this.Text = "Workshop_status";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbService_Type;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtActual_Porcent;
+    }
+}
